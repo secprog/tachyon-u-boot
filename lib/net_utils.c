832ad274cc29f731eb8799205b7db0401ddfc6f7
secprog <casimiro334@gmail.com>
2026-05-24 11:16:07 +0100

    fix: remove debug UART checks from IP checksum computation

--- lib/net_utils.c
+++ lib/net_utils.c
@@ -184,18 +184,6 @@ uint compute_ip_checksum(const void *vptr, uint nbytes)
 	int sum, oddbyte;
 	const unsigned short *ptr = vptr;
 
-	if (IS_ENABLED(CONFIG_DEBUG_UART) && (ulong)vptr < 0x1000) {
-		printascii("\n@@@ BAD IP CHECKSUM ptr=0x");
-		printhex8((ulong)vptr);
-		printascii(" len=0x");
-		printhex8(nbytes);
-		printascii(" lr=0x");
-		printhex8((ulong)__builtin_return_address(0));
-		printascii(" @@@\n");
-		for (;;)
-			;
-	}
-
 	sum = 0;
 	while (nbytes > 1) {
 		sum += *ptr++;
