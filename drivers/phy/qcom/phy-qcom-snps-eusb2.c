8a40b01efabdc0db1e523fafb723a762f415ba3d
secprog <casimiro334@gmail.com>
2026-05-27 20:00:34 +0100

    fix

--- drivers/phy/qcom/phy-qcom-snps-eusb2.c
+++ drivers/phy/qcom/phy-qcom-snps-eusb2.c
@@ -353,9 +353,6 @@ static const struct udevice_id qcom_snps_eusb2_phy_ids[] = {
 	{
 		.compatible = "qcom,sm8550-snps-eusb2-phy",
 	},
-	{
-		.compatible = "qcom,sc7280-usb-hs-phy",
-	},
 	{}
 };
 
